fa40a15a4e90722708d1b5e6387663391234572c
Alex Schlegel <schlegel@gmail.com>
2015-03-05 17:26:47 -0500

    Ravens scoring sub for Ali

---
+++ code/+MWL/+Rating/Ravens.m
@@ -0,0 +1,18 @@
+function score = Ravens(data,testType)
+% MWL.Rating.Ravens
+% 
+% Description:	score a Ravens test, given the subject responses
+% 
+% Syntax:	score = Ravens(data,testType)
+% 
+% In:
+% 	data		- an numerical array of the subject responses (1 through 8)
+%	testType	- the Ravens test type ('A' or 'B')
+% 
+% Out:
+% 	score	- the test score
+% 
+% Updated: 2015-03-05
+% Copyright 2015 Alex Schlegel (schlegel@gmail.com).  This work is licensed
+% under a Creative Commons Attribution-NonCommercial-ShareAlike 3.0 Unported
+% License.
